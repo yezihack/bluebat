ceef029f6969e9dc1e176e97652b8827002620e1
百里 <freeit@126.com>
2019-04-14 23:27:23 +0800

    delete log

--- log/201904/db_error.php
+++
@@ -1,2 +0,0 @@
-<?php exit;?>	2019-04-13 21:42:38	127.0.0.1	/install/action=db	0	SQL: errno: 1049, errstr: 连接数据库服务器失败:SQLSTATE[HY000] [1049] Unknown database 'xiuno4'
-<?php exit;?>	2019-04-14 12:35:10	127.0.0.1	/admin/?plugin-install-gmchina_xiuno_editormd.htm	1	SQL:ALTER TABLE bbs_post ADD `message_cache` longtext NOT NULL; errno: 1060, errstr: Duplicate column name 'message_cache'
